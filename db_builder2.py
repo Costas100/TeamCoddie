ca9246948f3a8bbb2764f0ae4be254552d6261fc
Costas100 <constantineathanitis@yahoo.com>
2017-02-07 13:12:53 -0500

    started

--- db_builder2.py
+++ db_builder2.py
@@ -2,52 +2,54 @@ from pymongo import MongoClient
 import csv
 
 #change from local server to Homer server
-server = MongoClient("127.0.0.1")
+#server = MongoClient("127.0.0.1")
 #server = MongoClient('homer.stuy.edu')
+server = MongoClient("149.89.150.100")
 
 #creating the database
 db = server.teamcoddiedb
 
 #creating the collection
-c = db.students
+studentCollection = db.students
 
 #opening up the csv/data files
 fObjP = open("peeps.csv")
-p=csv.DictReader(fObjP)
-
-fObjC = open("courses.csv")
-co = csv.DictReader(fObjC)
+people=csv.DictReader(fObjP)
 
 
-print p
-for i in p:
+
+
+
+for i in people:
     #creates a dictionary of the peeps data: name, age, id
     dPeeps = {"name":i["name"], "age":i["age"], "id":i["id"]}
 
-    for j in co:
+    fObjC = open("courses.csv")
+    courses = csv.DictReader(fObjC)
+
+    
+    for j in courses:
         #if id from peeps data matches with id from courses data,
         #add the mark for the specific course to the dictionary
         if j["id"] == dPeeps["id"]:
             dPeeps[j["code"]]= j["mark"]
             
-    print dPeeps
+#    print dPeeps
     #insert the dictionary/document into the collection
-    c.insert_one(dPeeps)
-    #after first insertion using local, it says that target machine actively refused connection
-    #after first insertion using homer, it says that the server time out
-
-#--------------testing out insertions of dictionaries--------
-'''
-num = 0
-c2 = db.numbers
-while num < 10:
-    numDict = {"number":num}
-    num=num+1
-    print numDict
-    c2.insert_one(numDict)
-    ##after the first insertion, it says that the server timed out 
-
-'''
+    studentCollection.insert_one(dPeeps)
 
 
+def averageMark(name):
+    currentStudent= server.db.studentCollection.find({"name":name})
+    print currentStudent
+   # softdev, systems, greatbooks, ceramics
+    for i in currentStudent:
+        print i
+        print
+        print
+
+#def displayAverage():
+    
+
+averageMark("kruder")
     
